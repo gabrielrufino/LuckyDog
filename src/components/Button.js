0708844a101c0a34f373fd0b1b421afacd135da7
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-11 19:58:18 -0300

    Outline e flat no botão

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {StyleSheet, Text, TouchableHighlight} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#C8AB83',
-    borderRadius: 5,
-    justifyContent: 'center',
-    paddingVertical: 14,
-    width: '100%',
-  },
-  label: {
-    color: '#FFFFFF',
-  },
-});
+export default function Button({flat, label, onPress, outlined}) {
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      borderRadius: 5,
+      justifyContent: 'center',
+      paddingVertical: 14,
+      width: '100%',
+      backgroundColor: '#C8AB83',
+      ...(outlined
+        ? {
+            backgroundColor: '#FFFFFF',
+            borderColor: '#C8AB83',
+            borderWidth: 2,
+          }
+        : {}),
+      ...(flat
+        ? {
+            backgroundColor: '#FFFFFF',
+          }
+        : {}),
+    },
+    label: {
+      fontWeight: 'bold',
+      color: outlined || flat ? '#C8AB83' : '#FFFFFF',
+    },
+  });
 
-export default function Button({label, onPress}) {
+  const underlayColor = useMemo(
+    () => (outlined || flat ? '#FFFFFF' : '#CACAAA'),
+    [flat, outlined],
+  );
+
   return (
     <TouchableHighlight
       activeOpacity={0.6}
-      underlayColor="#CACAAA"
+      underlayColor={underlayColor}
       style={styles.container}
       onPress={onPress}>
       <Text style={styles.label}>{label}</Text>
