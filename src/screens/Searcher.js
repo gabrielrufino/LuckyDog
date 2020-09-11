bc1f149ee5346eedd4bddd35009afa17cb147b00
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-11 20:01:00 -0300

    Tela de busca inicial

--- src/screens/Searcher.js
+++ src/screens/Searcher.js
@@ -1,11 +1,37 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Image, StyleSheet, View} from 'react-native';
 
+import Button from '../components/Button';
 import DogAPI from '../services/DogAPI';
+import Logo from '../assets/images/logo-500px.png';
+import Select from '../components/Select';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  spacer: {
+    paddingVertical: 5,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+});
 
 export default function Searcher() {
   const [breeds, setBreeds] = useState([]);
   const [loadingBreeds, setLoadingBreeds] = useState(true);
+  const [selectedBreed, setSelectedBreed] = useState('');
 
   useEffect(() => {
     DogAPI.get('/breeds/list/all')
@@ -17,9 +43,24 @@ export default function Searcher() {
       });
   }, []);
 
+  const onRiffle = useCallback(function () {}, []);
+
   return (
-    <View>
-      <Text>Searcher</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <View style={styles.logoContainer}>
+          <Image source={Logo} style={styles.logo} />
+        </View>
+        <View style={styles.spacer} />
+        <Select
+          options={breeds}
+          onSelect={setSelectedBreed}
+          selected={selectedBreed}
+        />
+        <View style={styles.spacer} />
+        <Button label="Sortear" onPress={onRiffle} />
+        <Button flat label="Estou com sorte" onPress={onRiffle} />
+      </View>
     </View>
   );
 }
