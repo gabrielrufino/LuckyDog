1c8548f0e8a9b4b4c00f5445ef9b518539a9a613
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-11 18:01:52 -0300

    Obtendo todas as raças de cachorro no searcher

--- src/screens/Searcher.js
+++ src/screens/Searcher.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 
+import DogAPI from '../services/DogAPI';
+
 export default function Searcher() {
+  const [breeds, setBreeds] = useState([]);
+  const [loadingBreeds, setLoadingBreeds] = useState(true);
+
+  useEffect(() => {
+    DogAPI.get('/breeds/list/all')
+      .then(({data: {message}}) => {
+        setBreeds(Object.keys(message));
+      })
+      .finally(() => {
+        setLoadingBreeds(false);
+      });
+  }, []);
+
   return (
     <View>
       <Text>Searcher</Text>
