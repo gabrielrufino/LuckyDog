336c9e0d973df6ecbb235a8946bbeffe88ce701f
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-11 21:14:50 -0300

    Setup da splash screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {StatusBar} from 'react-native';
 
 import Navigator from './Navigator';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor="#C8AB83" />
