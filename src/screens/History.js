ce0fad5179c2c0410376fc2d0c817409a933d2fa
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-14 15:22:00 -0300

    Feedback no histórico

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+import Header from '../components/Header';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+  },
+  warning: {
+    paddingHorizontal: 20,
+    textAlign: 'center',
+  },
+});
 
 export default function History() {
   return (
-    <View>
-      <Text>Histórico</Text>
+    <View style={styles.container}>
+      <Header title="Histórico" />
+
+      <Text style={styles.warning}>
+        Histórico ainda não desenvolvido :( Mas não desanime, não vamos
+        desenvolver!
+      </Text>
     </View>
   );
 }
