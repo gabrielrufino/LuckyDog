4ca294f345f4e8d50e98c42c7f9cd8c1818808c1
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-14 11:38:59 -0300

    Repositório para favoritos

---
+++ src/repositories/Repository.js
@@ -0,0 +1,13 @@
+export default class Repository {
+  async getAll() {
+    throw new Error('getAll not implemented');
+  }
+
+  async insertOne() {
+    throw new Error('insertOne not implemented');
+  }
+
+  async _initRepository() {
+    throw new Error('_initRepository not implemented');
+  }
+}
