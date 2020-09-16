6b02df9118fab41340623e81c1094b6a5a9ecf8c
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-16 14:56:50 -0300

    Remoção de importação não utilizada: useEffect

--- src/screens/Searcher.js
+++ src/screens/Searcher.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 
 import Button from '../components/Button';
