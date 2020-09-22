a74376246f092898f5e78b9e3047b24ab0243c44
Gabriel Rufino <contato@gabrielrufino.com>
2020-09-21 22:19:05 -0300

    Adição de ponto e vírgula

--- __tests__/components/DownloadButton-test.js
+++ __tests__/components/DownloadButton-test.js
@@ -2,7 +2,7 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import DownloadButton from '../../src/components/DownloadButton'
+import DownloadButton from '../../src/components/DownloadButton';
 
 describe('components/DownloadButton.js', () => {
   it('Should renders the download button correctly', () => {
